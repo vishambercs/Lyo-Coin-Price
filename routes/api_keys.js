0b7caeddf34852d5b81002ba61438ec7c0c8021d
Vishamber Lal <v.c@vaimanagement.co>
2022-07-14 15:13:30 +0400

    Changes

--- routes/api_keys.js
+++ routes/api_keys.js
@@ -19,6 +19,8 @@ module.exports =
 {
     async get_price(req, res) {
         try {
+            var d1 = new Date();
+            var d2 = new Date()
             var lyo_api_url = "https://openapi.lyotrade.com/sapi/v1/ticker?symbol="
             var symbol = "lyo1usdt";
             lyo_api_url += symbol
@@ -26,67 +28,126 @@ module.exports =
             var lyo_pay_data = JSON.parse(response.data).data
             var type = ""
             var id = ""
+           
+            var msDiff =  ((d1 - d2)/60000) ;
             if (req.query.symbol == "lyoaed") {
                 id = 7
                 type = "AED";
+                rate = 3.67 
+                let price_object = [{
+                    "id": id,
+                    "symbol": req.query.symbol,
+                    high: (parseFloat(rate) * parseFloat(lyo_pay_data.high)),
+                    vol: (parseFloat(rate) * parseFloat(lyo_pay_data.vol)),
+                    last: (parseFloat(rate) * parseFloat(lyo_pay_data.last)),
+                    low: (parseFloat(rate) * parseFloat(lyo_pay_data.low)),
+                    buy: (parseFloat(rate) * parseFloat(lyo_pay_data.buy)),
+                    sell: (parseFloat(rate) * parseFloat(lyo_pay_data.sell)),
+                    rose: (parseFloat(rate) * parseFloat(lyo_pay_data.rose)),
+                    time: (parseFloat(rate) * parseFloat(lyo_pay_data.time)),
+                    open: (parseFloat(rate) * parseFloat(lyo_pay_data.open)),
+                    "msDiff" : msDiff   
+                }]
+                json_varable = price_object
+                res.json(price_object)
             }
             else if (req.query.symbol == "lyoeur") {
                 id = 8
                 type = "EUR";
+                rate = 3.68
+                let price_object = [{
+                    "id": id,
+                    "symbol": req.query.symbol,
+                    high: (parseFloat(rate) * parseFloat(lyo_pay_data.high)),
+                    vol: (parseFloat(rate) * parseFloat(lyo_pay_data.vol)),
+                    last: (parseFloat(rate) * parseFloat(lyo_pay_data.last)),
+                    low: (parseFloat(rate) * parseFloat(lyo_pay_data.low)),
+                    buy: (parseFloat(rate) * parseFloat(lyo_pay_data.buy)),
+                    sell: (parseFloat(rate) * parseFloat(lyo_pay_data.sell)),
+                    rose: (parseFloat(rate) * parseFloat(lyo_pay_data.rose)),
+                    time: (parseFloat(rate) * parseFloat(lyo_pay_data.time)),
+                    open: (parseFloat(rate) * parseFloat(lyo_pay_data.open)),
+                    "msDiff" : msDiff   
+                }]
+                json_varable = price_object
+                res.json(price_object)
             }
             else if (req.query.symbol == "lyousd") {
                 id = 9
                 type = "USD";
+                rate = 1
+                let price_object = [{
+                    "id": id,
+                    "symbol": req.query.symbol,
+                    high: (parseFloat(rate) * parseFloat(lyo_pay_data.high)),
+                    vol: (parseFloat(rate) * parseFloat(lyo_pay_data.vol)),
+                    last: (parseFloat(rate) * parseFloat(lyo_pay_data.last)),
+                    low: (parseFloat(rate) * parseFloat(lyo_pay_data.low)),
+                    buy: (parseFloat(rate) * parseFloat(lyo_pay_data.buy)),
+                    sell: (parseFloat(rate) * parseFloat(lyo_pay_data.sell)),
+                    rose: (parseFloat(rate) * parseFloat(lyo_pay_data.rose)),
+                    time: (parseFloat(rate) * parseFloat(lyo_pay_data.time)),
+                    open: (parseFloat(rate) * parseFloat(lyo_pay_data.open)),
+                    "msDiff" : msDiff   
+                }]
+                json_varable = price_object
+                res.json(price_object)
             }
-            var d1 = new Date(); // 2014-01-20 12:30:00
+            else{
+                
+                res.json(json_varable)
+            }
+            // 2014-01-20 12:30:00
            
                
             // Difference between d2 and d1 in milliseconds (50400000)
-            var msDiff =  ((d1 - d2)/60000) ;
-            if(msDiff > 60 || json_varable[0].id == 0  ){
-            d2 = new Date()
-            var config = {
-                method: 'GET',
-                url: 'https://api.apilayer.com/exchangerates_data/convert?to=' + type + '&from=USD&amount=1',
-                headers: {
-                    'apikey': 'wRQcpTwedoJ68iv5ibftbEmo8p1kDlq8'
-                },
-            };
-            var rate = 0;
-            await axios(config)
-                .then(function (response) {
-                    // console.log(JSON.stringify());
-                    if (response.data.success == true) {
-                        rate = response.data.info.rate
-                        let price_object = [{
-                            "id": id,
-                            "symbol": req.query.symbol,
-                            high: (parseFloat(rate) * parseFloat(lyo_pay_data.high)),
-                            vol: (parseFloat(rate) * parseFloat(lyo_pay_data.vol)),
-                            last: (parseFloat(rate) * parseFloat(lyo_pay_data.last)),
-                            low: (parseFloat(rate) * parseFloat(lyo_pay_data.low)),
-                            buy: (parseFloat(rate) * parseFloat(lyo_pay_data.buy)),
-                            sell: (parseFloat(rate) * parseFloat(lyo_pay_data.sell)),
-                            rose: (parseFloat(rate) * parseFloat(lyo_pay_data.rose)),
-                            time: (parseFloat(rate) * parseFloat(lyo_pay_data.time)),
-                            open: (parseFloat(rate) * parseFloat(lyo_pay_data.open)),
-                            "msDiff" : msDiff   
-                        }]
-                        json_varable = price_object
-                        res.json(price_object)
-                    }
-                })
-                .catch(function (error) {
-                    console.log(error);
-                    res.json({})
-                });
+            // var msDiff =  ((d1 - d2)/60000) ;
+            // var dif = (d1.getTime() - d2.getTime()) / 1000;
+            // console.log(dif)
+            // if(msDiff > 60 || json_varable[0].id == 0  ){
+            // d2 = new Date()
+            // var config = {
+            //     method: 'GET',
+            //     url: 'https://api.apilayer.com/exchangerates_data/convert?to=' + type + '&from=USD&amount=1',
+            //     headers: {
+            //         'apikey': 'wRQcpTwedoJ68iv5ibftbEmo8p1kDlq8'
+            //     },
+            // };
+            // var rate = 0;
+            // await axios(config)
+            //     .then(function (response) {
+            //         // console.log(JSON.stringify());
+            //         if (response.data.success == true) {
+            //             rate = response.data.info.rate
+            //             let price_object = [{
+            //                 "id": id,
+            //                 "symbol": req.query.symbol,
+            //                 high: (parseFloat(rate) * parseFloat(lyo_pay_data.high)),
+            //                 vol: (parseFloat(rate) * parseFloat(lyo_pay_data.vol)),
+            //                 last: (parseFloat(rate) * parseFloat(lyo_pay_data.last)),
+            //                 low: (parseFloat(rate) * parseFloat(lyo_pay_data.low)),
+            //                 buy: (parseFloat(rate) * parseFloat(lyo_pay_data.buy)),
+            //                 sell: (parseFloat(rate) * parseFloat(lyo_pay_data.sell)),
+            //                 rose: (parseFloat(rate) * parseFloat(lyo_pay_data.rose)),
+            //                 time: (parseFloat(rate) * parseFloat(lyo_pay_data.time)),
+            //                 open: (parseFloat(rate) * parseFloat(lyo_pay_data.open)),
+            //                 "msDiff" : msDiff   
+            //             }]
+            //             json_varable = price_object
+            //             res.json(price_object)
+            //         }
+            //     })
+            //     .catch(function (error) {
+            //         console.log(error);
+            //         res.json({})
+            //     });
 
-            }
-            else
-            {
-                json_varable[0]["msDiff"] = msDiff
-                res.json(json_varable)  
-            }
+            // }
+            // else
+            // {
+            //     json_varable[0]["msDiff"] = msDiff
+            //     res.json(json_varable)  
+            // }
 
 
 
